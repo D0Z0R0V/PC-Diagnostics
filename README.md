b12639ba52aabb6af593d5327aac75931961b677
D0Z0R0V <denielmorozov8000@gmail.com>
2025-01-26 22:14:24 +0300

    Updates to the main me and image processing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PC-Diagnostics
+## ⚙️ Installation
+It will appear after the creation of the main functionality
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,92 +1,198 @@
-import sys
 from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QLabel, QMenuBar, QVBoxLayout, QWidget, QPushButton
+    QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel,
+    QPushButton, QWidget, QStackedWidget, QListWidget, QGridLayout, QListWidgetItem, QMenu
 )
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QPixmap, QAction, QIcon
+from PyQt6.QtCore import Qt
+from PyQt6.QtCore import QSize
+
+import sys
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Диагностика системы")
-        self.setGeometry(100, 100, 800, 600)
+        self.setGeometry(100, 100, 1200, 800)
 
-        # Главное содержимое окна
-        self.main_label = QLabel("Добро пожаловать в диагностическую систему!", self)
-        self.main_label.setGeometry(50, 50, 700, 30)
+        main_widget = QWidget()
+        main_layout = QHBoxLayout(main_widget)
 
-        # Создание верхнего меню
-        self.menu_bar = self.menuBar()
+        self.menu_list = QListWidget()
+        self.menu_list.setMaximumWidth(200)
+        self.menu_list.itemClicked.connect(self.switch_screen)
+        self.init_menu_list()
+        main_layout.addWidget(self.menu_list)
 
-        # Меню "Редактирование"
-        edit_menu = self.menu_bar.addMenu("Редактирование")
-        edit_action1 = QAction("Test1", self)
-        edit_action2 = QAction("Test2", self)
-        edit_action3 = QAction("Test3", self)
-        edit_menu.addAction(edit_action1)
-        edit_menu.addAction(edit_action2)
-        edit_menu.addAction(edit_action3)
+        self.stacked_widget = QStackedWidget()
+        main_layout.addWidget(self.stacked_widget)
+        self.init_screens()
 
-        # Меню "Настройка"
-        settings_menu = self.menu_bar.addMenu("Настройка")
-        settings_action1 = QAction("Test1", self)
-        settings_action2 = QAction("Test2", self)
-        settings_action3 = QAction("Test3", self)
-        settings_menu.addAction(settings_action1)
-        settings_menu.addAction(settings_action2)
-        settings_menu.addAction(settings_action3)
+        self.setCentralWidget(main_widget)
+        self.create_menus()
 
-        # Меню "О приложении"
-        about_menu = self.menu_bar.addMenu("О приложении")
-        about_action = QAction("О приложении", self)
+    def init_menu_list(self):
+        """Инициализация левого меню с иконками."""
+        menu_items = [
+            ("Общие сведения", "icons/general.png"),
+            ("Процессор", "icons/cpu.png"),
+            ("Оперативная память", "icons/ram.png"),
+            ("Дисковая подсистема", "icons/disk.png"),
+            ("Видеокарта", "icons/gpu.png"),
+            ("Материнская плата", "icons/motherboard.png"),
+            ("Напряжение", "icons/voltage.png"),
+            ("Диагностика", "icons/diagnostic.png"),
+            ("Тестирование", "icons/testing.png"),
+        ]
+
+        for text, icon_path in menu_items:
+            item = QListWidgetItem()
+            item.setText(text)
+            item.setIcon(QIcon(icon_path))
+            self.menu_list.addItem(item)
+
+    def create_menus(self):
+        """Создаем верхнее меню приложения."""
+        menu_bar = self.menuBar()
+
+        # Файл
+        file_menu = menu_bar.addMenu("Файл")
+        file_menu.addAction(QAction("Text 1", self))
+        file_menu.addAction(QAction("Text 2", self))
+
+        # Настройки
+        settings_menu = menu_bar.addMenu("Настройки")
+        settings_menu.addAction(QAction("Text 3", self))
+        settings_menu.addAction(QAction("Text 4", self))
+
+        # Отчет
+        report_menu = menu_bar.addMenu("Отчет")
+        report_menu.addAction(QAction("Text 5", self))
+
+        # Помощь
+        help_menu = menu_bar.addMenu("Помощь")
+        help_menu.addAction(QAction("Text 6", self))
+        help_menu.addAction(QAction("Text 7", self))
+
+        # О приложении
+        about_menu = menu_bar.addMenu("О приложении")
+        about_action = QAction("О программе", self)
         about_action.triggered.connect(self.show_about)
         about_menu.addAction(about_action)
 
-        # Кнопка перехода на новый экран
-        self.transition_button = QPushButton("Перейти на следующий экран", self)
-        self.transition_button.setGeometry(50, 100, 300, 40)
-        self.transition_button.clicked.connect(self.open_new_screen)
+    def init_screens(self):
+        """Инициализация экранов для QStackedWidget."""
+        self.general_info_screen = self.create_general_screen()
+        self.processor_screen = self.create_info_screen("Процессор", "Информация о процессоре.", "gui/img/cpu.png")
+        self.memory_screen = self.create_info_screen("Оперативная память", "Информация о памяти.", "gui/img/ram.png")
+        self.disk_screen = self.create_info_screen("Дисковая подсистема", "Информация о дисках.", "gui/img/disk.png")
+        self.gpu_screen = self.create_info_screen("Видеокарта", "Информация о видеокарте.", "gui/img/gpu.png")
+        self.motherboard_screen = self.create_info_screen("Материнская плата", "Информация о материнской плате.", "gui/img/motherboard.png")
+        self.voltage_screen = self.create_info_screen("Напряжение", "Информация о напряжении.")
+        self.diagnostic_screen = self.create_info_screen("Диагностика", "Режим диагностики системы.")
+        self.testing_screen = self.create_info_screen("Тестирование", "Тестирование системы на устойчивость.")
+        
+        self.stacked_widget.addWidget(self.general_info_screen)
+        self.stacked_widget.addWidget(self.processor_screen)
+        self.stacked_widget.addWidget(self.memory_screen)
+        self.stacked_widget.addWidget(self.disk_screen)
+        self.stacked_widget.addWidget(self.gpu_screen)
+        self.stacked_widget.addWidget(self.motherboard_screen)
+        self.stacked_widget.addWidget(self.voltage_screen)
+        self.stacked_widget.addWidget(self.diagnostic_screen)
+        self.stacked_widget.addWidget(self.testing_screen)
+        self.stacked_widget.setCurrentWidget(self.general_info_screen)
+
+    def create_general_screen(self):
+        """Создаем экран 'Общие сведения' с сеткой карточек."""
+        screen = QWidget()
+        layout = QGridLayout(screen)
+
+        cards = [
+            ("Процессор", "gui/img/cpu.png"),
+            ("Оперативная память", "gui/img/ram.png"),
+            ("Дисковая подсистема", "gui/img/disk.png"),
+            ("Видеокарта", "gui/img/gpu.png"),
+            ("Материнская плата", "gui/img/motherboard.png"),
+            ("Напряжение", None),
+            ("Диагностика", None),
+            ("Тестирование", None),
+        ]
+
+        for i, (title, icon_path) in enumerate(cards):
+            card = QPushButton()
+            card.setText(title)
+            card.setStyleSheet("text-align: center;")
+            if icon_path:
+                card.setIcon(QIcon(icon_path))
+                card.setIconSize(QSize(80, 80))
+            card.clicked.connect(lambda _, t=title: self.switch_screen_by_title(t))
+            layout.addWidget(card, i // 4, i % 4)
+
+        return screen
+
+    def create_info_screen(self, title, description, image_path=None):
+        """Создаем экран для QStackedWidget."""
+        screen = QWidget()
+        layout = QVBoxLayout(screen)
+
+        title_label = QLabel(f"<h1>{title}</h1>", self)
+        layout.addWidget(title_label, alignment=Qt.AlignmentFlag.AlignTop)
+
+        if image_path:
+            image_label = QLabel(self)
+            pixmap = QPixmap(image_path)
+            if pixmap.isNull():
+                print(f"Ошибка: не удалось загрузить изображение по пути: {image_path}")
+                return screen
+            resized_pixmap = pixmap.scaled(80, 80, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+            image_label.setPixmap(resized_pixmap)
+            layout.addWidget(image_label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        description_label = QLabel(description, self)
+        layout.addWidget(description_label, alignment=Qt.AlignmentFlag.AlignTop)
+
+        return screen
+
+    def switch_screen(self, item):
+        """Переключение экранов по выбору в боковом меню."""
+        screens = {
+            "Общие сведения": self.general_info_screen,
+            "Процессор": self.processor_screen,
+            "Оперативная память": self.memory_screen,
+            "Дисковая подсистема": self.disk_screen,
+            "Видеокарта": self.gpu_screen,
+            "Материнская плата": self.motherboard_screen,
+            "Напряжение": self.voltage_screen,
+            "Диагностика": self.diagnostic_screen,
+            "Тестирование": self.testing_screen
+        }
+        selected_screen = screens.get(item.text())
+        if selected_screen:
+            self.stacked_widget.setCurrentWidget(selected_screen)
+
+    def switch_screen_by_title(self, title):
+        """Переключение экранов по нажатию на карточку."""
+        screens = {
+            "Общие сведения": self.general_info_screen,
+            "Процессор": self.processor_screen,
+            "Оперативная память": self.memory_screen,
+            "Дисковая подсистема": self.disk_screen,
+            "Видеокарта": self.gpu_screen,
+            "Материнская плата": self.motherboard_screen,
+            "Напряжение": self.voltage_screen,
+            "Диагностика": self.diagnostic_screen,
+            "Тестирование": self.testing_screen
+        }
+        selected_screen = screens.get(title)
+        if selected_screen:
+            self.stacked_widget.setCurrentWidget(selected_screen)
 
     def show_about(self):
-        """Отобразить окно 'О приложении'."""
-        self.main_label.setText("Это приложение для диагностики системы.\nРазработано для дипломного проекта.")
-        self.main_label.adjustSize()
-
-    def open_new_screen(self):
-        """Переход на новый экран."""
-        self.new_screen = SecondScreen(self)
-        self.setCentralWidget(self.new_screen)
-
-    def return_to_main_screen(self):
-        """Вернуться на главный экран."""
-        self.setCentralWidget(None)  # Удаляем текущий виджет
-        self.main_label.show()
-        self.transition_button.show()
+        """Отображение информации 'О приложении'."""
+        about_text = "Это приложение для диагностики системы.\nРазработано для дипломного проекта."
+        self.general_info_screen.layout().itemAt(1).widget().setText(about_text)
+        self.stacked_widget.setCurrentWidget(self.general_info_screen)
 
 
-class SecondScreen(QWidget):
-    def __init__(self, main_window):
-        super().__init__()
-        self.main_window = main_window
 
-        layout = QVBoxLayout()
-
-        label = QLabel("Это второй экран приложения", self)
-        layout.addWidget(label)
-
-        back_button = QPushButton("Вернуться на главный экран", self)
-        back_button.clicked.connect(self.return_to_main_screen)
-        layout.addWidget(back_button)
-
-        self.setLayout(layout)
-
-    def return_to_main_screen(self):
-        """Возврат на главный экран через главное окно."""
-        self.main_window.return_to_main_screen()
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
